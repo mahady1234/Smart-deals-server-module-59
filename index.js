a50930932bd2b2044d331d1e4990c80426a0e2a5
My-first-github-repo <khanrumi195@gmail.com>
2025-11-30 18:31:36 +0600

    final commit

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.get('/', (req, res) => {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         const db = client.db('smart-db')
         const productCollection = db.collection("products")
@@ -283,7 +283,7 @@ async function run() {
             res.send(result)
         })
 
-       
+
 
 
 
